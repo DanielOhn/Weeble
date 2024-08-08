fb0031bf0383785b307b92e1240ab31616e3fda2
catsby <ohndaniel@gmail.com>
2024-08-07 23:59:43 -0700

    Remove early build and development portions of the code.  Fix mp4 not showing the correct daily weeble.

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import animeNames from "./anime-name-list.json";
 
 import ReactPlayer from 'react-player'
 
-interface AnimeInfo {
-  id: number;
-  name: string;
+interface Weeble {
+  name: any;
+  vid_list: any[] | undefined;
 }
 
 
@@ -18,41 +18,27 @@ function App() {
   const [guess, setGuess] = useState<string>("")
   const [guesses, setGuesses] = useState<number>(0)
 
-  const [dailyWeeble, setDailyWeeble] = useState<string>("")
+  const [dailyWeeble, setDailyWeeble] = useState<any>()
   const [video, setVideo] = useState<string>("")
   const [vidNum, setVidNum] = useState<number>(0)
+
+  const [weeble, setWeeble] = useState<Weeble | undefined>(undefined)
 
 
   const [page, setPage] = useState<number>(1)
   const [anime, setAnime] = useState<any[] | undefined>()
 
-  const [output, setOutput] = useState<string>("")
+  // const [output, setOutput] = useState<string>("")
 
   const [animeOptions, setAnimeOptions] = useState<any>()
-
-
-  // var query = `
-  // query ($id: Int) { # Define which variables will be used in the query (id)
-  //   Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-  //     id
-  //     title {
-  //       romaji
-  //       english
-  //       native
-  //     }
-
-  //   }
-  // }
-  // `;
-
-  // Define our query variables and values that will be used in the query request
 
   function handleResponse(response: any) {
     return response.json().then(function (json: any) {
       //return response.ok ? json : Promise.reject(json);
       //console.log(json)
       if (response.ok) {
-        setAnime(json)
+        console.log("JSON Data: ", json)
+        setWeeble({"name": weeble?.name, "vid_list": json})
       } else {
         console.log(response)
       }
@@ -64,14 +50,44 @@ function App() {
     console.error(error);
   }
 
-  const setupOutput = () => {
-    setOutput("{")
-  }
+  // const setupOutput = () => {
+  //   setOutput("{")
+  // }
 
   //var fix_name
 
+  useEffect(() => {
+    if (weeble != undefined) {
+      var url = `/post.json?tags=${weeble.name.name}`;
+      console.log("TAG: ", weeble.name.name)
+
+      var options = {
+        method: 'GET',
+        mode: "cors" as RequestMode,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      };
+  
+  
+      const fetch_data = () => {
+        try {
+          fetch(url, options)
+            .then(handleResponse)
+            .catch(handleError);
+        }
+        catch (err) {
+          alert(err);
+        }
+      }
+  
+      fetch_data()
+    }
+  }, [weeble?.name])
+
   // TO FETCH POST
-  const generateContent = () => {
+  const generateWeeble = () => {
     let anime_list = animeData.Anime
     console.log("ANIME LIST: " + JSON.stringify(anime_list.length))
     //console.log(anime_list)
@@ -80,164 +96,39 @@ function App() {
     //   console.log(anim)
     // })
 
-    let daily_weeb = anime_list[(Math.floor(Math.random() * anime_list.length))]
+    let get_daily_weeb = anime_list[(Math.floor(Math.random() * anime_list.length))]
     //console.log("Daily Weeb: " + JSON.stringify(daily_weeb))
 
-
-    var url = `/post.json?tags=${daily_weeb.name}`;
-
-    var options = {
-      method: 'GET',
-      mode: "cors" as RequestMode,
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-      // body: JSON.stringify({
-      //     query: query,
-      //     variables: variables
-      // })
-    };
-
-
-    const fetch_data = () => {
-      try {
-        fetch(url, options).then(handleResponse)
-          .catch(handleError);
-      }
-      catch (err) {
-        alert(err);
-      }
-    }
-
-    fetch_data()
-    //console.log("Get Anime: " + JSON.stringify(anime_list))
-
-
-    let tempStr = daily_weeb.name.replaceAll("_", " ")
-    let finalWeeb = tempStr;
-    console.log("TEMP: " + tempStr)
-    for (let i = 0; i < finalWeeb.length; i++) {
-
-      if (i === 0 || finalWeeb[i - 1] === " ") {
-        //console.log(finalWeeb.charAt(i).toUpperCase())
-        let char = finalWeeb.charAt(i).toUpperCase()
-        finalWeeb = finalWeeb.substring(0, i) + char + finalWeeb.substring(i + 1)
-      }
-    }
-
-    console.log("Final Weeb: " + finalWeeb)
-
-    if (finalWeeb) {
-      setDailyWeeble(finalWeeb)
-
-    }
-
-    //setDailyWeeble("Testing")
-    if (anime) {
-      setVideo(anime[0].file_url)
-    }
+    setWeeble({"name": get_daily_weeb, "vid_list": undefined})
 
   }
 
-  useEffect(() => {
-    if (anime) {
-      setVideo(anime[vidNum].file_url)
+  const updateVideo = () => {
+    if (weeble?.vid_list != undefined)
+      setVideo(weeble.vid_list[vidNum].file_url)
     }
-  }, [anime, vidNum])
 
   useEffect(() => {
-    setupOutput()
-    //console.log(output)
-    generateContent()
+    if (weeble?.vid_list != undefined) {
+      updateVideo()
+    }
+    
+  }, [weeble?.vid_list, vidNum])
+
+  // STARTING USE EFFECT TO GENERATE THE DAILY WEEBLE
+  useEffect(() => {
+
+    generateWeeble()
   }, [])
 
-  // TO FETCH TAGS
-  // useEffect(()  => {
 
-  //   var url = `/tag.json?type=3&limit=8&order=count&page=${page}`;
-
-  //   var options = {
-  //     method: 'GET',
-  //     mode: "cors" as RequestMode,
-  //     headers: {
-  //         'Content-Type': 'application/json',
-  //         'Accept': 'application/json'
-  //     }
-  //   };
-
-  //   const fetch_data = () => {
-  //       try {
-  //         fetch(url, options).then(handleResponse)
-  //           .catch(handleError);
-  //       }
-  //       catch(err) {
-  //         alert(err);
-  //       }
-  //     }
-
-  //     //fetch_data()
-  // }, [page])
-
-  const nextPage = () => {
-    setPage(page + 1)
-    //console.log(`Page #: ${page}`)
-  }
-
-  const toText = () => {
-    var res: string = `{ "Anime": [`
-    if (anime) {
-      anime.forEach((tag, ind) => {
-
-        res += `{"id": ${tag.id}, "name": "${tag.name}"}`
-        if (anime.length - 1 != ind) {
-          res += ", "
-        }
-      });
-    }
-
-    //console.log(res)
-
-    setOutput(res)
-  }
-
-  // const extractNames = () => {
-  //   let nameResult: string = `{ "anime_names": [`
-
-  //   if (anime) {
-  //     anime.forEach((ani) => {
-  //       let tempStr = ani.name
-  //       tempStr = tempStr.replaceAll("_", " ")
-
-  //       for (let i = 0; i < tempStr.length; i++) {
-  //         if (i === 0 || tempStr[i - 1] === " ") {
-  //           let char = tempStr.charAt(i).toUpperCase()
-  //           tempStr = tempStr.substring(0,i) + char + tempStr.substring(i+1)
-  //         }
-  //       }
-  //       //console.log(tempStr)
-
-  //       nameResult += `"${tempStr}",`
-
-  //       //nameResult += tempStr + "\n"
-  //     })
-
-  //   }
-  //   let finalResult = nameResult.slice(0, -1) + "]}"
-  //   //nameResult.charAt(nameResult.length) = "}"
-
-  //   //console.log(finalResult)
-
-  //   const file = new Blob([finalResult], { type: 'text/plain;charset=utf-8' });
-  //   saveAs(file, "anime-name-list.json")
-
-  // }
+  // RETURNS ANIME OPTIONS/ SETS IT UP
   useEffect(() => {
     const extractNames = () => {
 
       if (animeData.Anime) {
         let data = animeData.Anime
-        console.log(data)
+        //console.log(data)
   
   
         return (
@@ -253,7 +144,7 @@ function App() {
               }
               
             }
-            console.log("Temp String: " + tempStr) 
+            //console.log("Temp String: " + tempStr) 
             return (
               <option key={ani.id} value={tempStr}></option>
             )
@@ -271,71 +162,72 @@ function App() {
   
 
 
-  const printAnime = () => {
-    console.log(anime)
-
-    //const file = new Blob(['Hello, world!'], { type: 'text/plain;charset=utf-8' });
-    // saveAs(file)
-  }
-
   const showWeeble = () => {
-    console.log(dailyWeeble)
-  }
-
-  const printOutput = () => {
-    console.log(output)
-  }
-
-  const saveFile = () => {
-    setOutput(output + "]}")
-    const file = new Blob([output], { type: 'text/plain;charset=utf-8' });
-    saveAs(file, "sakugabooru-list.json")
+    console.log(weeble)
   }
 
   const guessAnime = () => {
-    console.log(guess)
-    if (guess == dailyWeeble) {
-      alert("WIN!")
-      // unlock all the vids/guesses
-    }
+    if (weeble) {
+      let checkString = weeble.name.name
 
-    setGuesses(guesses + 1)
+      let res = guess.replaceAll(" ", "_")
+      console.log("RES: ", res.toUpperCase())
+      console.log("CHECK: ", checkString)
 
-    if (guesses >= 5) {
-      alert("u lose")
-    } else {
-      setVidNum(vidNum + 1)
+      res = res.toUpperCase()
+
+      checkString = checkString.toUpperCase()
+
+      if (res === checkString) {
+        alert("WIN!")
+        // unlock all the vids/guesses
+      }
+      
+      if (guesses === 6) {
+        alert("u lose")
+      } else {
+        itrVidNum(1)
+        setGuesses(guesses + 1)
+      }
     }
   }
+  const itrVidNum = (num: number) => {
 
-  useEffect(() => {
-    const loadAnimeOptions = () => {
-      // load text file 
-      //console.log(animeNames)
-      let names: any = animeNames;
-      //console.log(names.anime_names)
-
-      // for (let i = 0; i < names.anime_names.length; i++) {
-      //   console.log(names.anime_names[i])
-
-      // names.anime_names.map((name: any) => {
-      //   //console.log(name)
-      // })
-
-      // }
-      return (
-        <div>
-          {names.anime_names.map((name: any) => {
-            //console.log(name)
-            return <option key={name} value={name}></option>
-
-          })}
-        </div>
-      )
+    if (vidNum === 0 && num === -1 || vidNum === 6 && num === 1) {
+      return;
     }
 
-    //setAnimeOptions(loadAnimeOptions)
-  }, [animeNames])
+    setVidNum(vidNum + num)
+    
+  }
+
+  // const showStates = (): any => {
+  //   if (anime && dailyWeeble) {
+  //     return <p>Anime: {anime} - Weeble: {dailyWeeble}</p>
+  //   }
+  //   return <p></p>
+  // }
+
+  // Sets up 
+  // useEffect(() => {
+  //   const loadAnimeOptions = () => {
+  //     // load text file 
+  //     //console.log(animeNames)
+  //     let names: any = animeNames;
+
+  //     return (
+  //       <div>
+  //         {names.anime_names.map((name: any) => {
+  //           //console.log(name)
+  //           return <option key={name} value={name}></option>
+
+  //         })}
+  //       </div>
+  //     )
+  //   }
+
+  //   //setAnimeOptions(loadAnimeOptions)
+  // }, [animeNames])
 
 
 
@@ -346,28 +238,14 @@ function App() {
         <ReactPlayer url={video} width="640px" height="360px" controls={true} />
       </header>
 
-      {/* <div>
-        <button className="btn" onClick={nextPage}>
-          Next Page
+      <div >
+        <button onClick={() => itrVidNum(-1)}>
+          {"<"}
+        </button>      
+        <button onClick={() => itrVidNum(1)}>
+          {">"}
         </button>
-        <button className="btn" onClick={toText}>
-          Convert To Text
-        </button><br />
-
-        <button className="btn" onClick={printAnime}>
-          Print Anime
-        </button>
-        <button className="btn" onClick={printOutput}>
-          Print Text Output
-        </button> */}
-      {/* <button className="btn" onClick={extractNames}>
-        Extract Names
-      </button><br /> */}
-      {/* 
-        <button className="btn" onClick={saveFile}>
-          Save
-        </button>
-      </div> */}
+      </div>
 
       <button onClick={showWeeble}>
         Show Weeble
